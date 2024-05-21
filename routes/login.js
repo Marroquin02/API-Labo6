1fbedea20579ef2b5d213ffc4d7983da4ad87240
Marroquin02 <00025221@uca.edu.sv>
2024-05-21 16:37:35 -0600

    Fix

--- routes/login.js
+++ routes/login.js
@@ -14,33 +14,33 @@ router.post("/", verifyLogin, async (req, res) => {
   res.json({ token });
 });
 
-// router.post("/add", async (req, res) => {
-//   const { users } = req.body;
-//   const cliente = client();
-//   try {
-//     await cliente.connect();
-//     const database = cliente.db(process.env.MONGO_DBNAME);
-//     const Users = database.collection("Users");
-//     console.log(users.length)
-//     for (let i = 0; i < users.length; i++) {
-//       const salt = bcrypt.genSaltSync(10);
-//       await Users.insertOne({
-//         carnet: users[i].carnet,
-//         nombre: users[i].nombre,
-//         correo: users[i].correo,
-//         password: bcrypt.hashSync(users[i].password, salt),
-//       });
-//     }
-//     res.json({
-//       status: "success",
-//       message: "Users added successfully",
-//     });
-//   } catch (error) {
-//     console.log(error);
-//     return res.status(401).json({ message: "Invalid Credential" });
-//   } finally {
-//     await cliente.close();
-//   }
-// });
+router.post("/add", async (req, res) => {
+  const { users } = req.body;
+  const cliente = client();
+  try {
+    await cliente.connect();
+    const database = cliente.db(process.env.MONGO_DBNAME);
+    const Users = database.collection("Users");
+    console.log(users.length)
+    for (let i = 0; i < users.length; i++) {
+      const salt = bcrypt.genSaltSync(10);
+      await Users.insertOne({
+        carnet: users[i].carnet,
+        nombre: users[i].nombre,
+        correo: users[i].correo,
+        password: bcrypt.hashSync(users[i].password, salt),
+      });
+    }
+    res.json({
+      status: "success",
+      message: "Users added successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(401).json({ message: "Invalid Credential" });
+  } finally {
+    await cliente.close();
+  }
+});
 
 module.exports = router;
